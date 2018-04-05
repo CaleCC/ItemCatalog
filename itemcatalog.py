0091f592b13aa7ddfb4190e5011367ef5f054431
Cheng Chen <chencheng502@gmail.com>
2018-04-05 00:45:32 -0400

    put links  for edit and delete category in homepage

--- itemcatalog.py
+++ itemcatalog.py
@@ -12,7 +12,7 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/catalog')
+@app.route('/catalog/')
 def getCatalog():
     catalog = session.query(Category).all()
     return render_template("catalog.html", catalogs = catalog)
@@ -20,7 +20,7 @@ def getCatalog():
 
 
 @app.route('/catalog/<int:category_id>/items')
-def getItemsOfCatalog(category_id):
+def getCategory(category_id):
     catalog = session.query(Category).filter_by(id = category_id).one()
     items = session.query(Item).filter_by(category_id = category_id).all()
     return render_template("items.html", catalog = catalog.name, items = items)
@@ -34,41 +34,62 @@ def getItemInformation(item_id):
     return render_template("item.html", catalog = catalog, item = item)
     #return 'show description of an item %d that belong to category %d' % item_id, category_id
 
-#add a new catalog
-@app.route('/catalog/new', method=['GET','POST'])
+#add a new category
+@app.route('/catalog/new/', methods=['GET','POST'])
 def addCatalog():
     if request.method == 'POST':
-        newCatalog = Catalog(name=request.form['name'])
-        session.add(newRestaurant)
+        newCatalog = Category(name=request.form['name'])
+        session.add(newCatalog)
         session.commit()
-        return redirect(url_for(getCatalog))
+        return redirect(url_for('getCatalog'))
     else:
-        return render_template('newCatalog.html')
-    return 'add a new catalog'
+        return render_template('newCategory.html')
 
 #should allow delete of a category?
-@app.route('/catalog/<int:category_id>/delete/' methods=['GET','POST'])
-def deleteCatalog(category_id):
-    return 'delete category ' + category_id
+@app.route('/catalog/<int:category_id>/delete/', methods=['GET','POST'])
+def deleteCategory(category_id):
+    category = session.query(Category).filter_by(id=category_id).one()
+    if request.method == 'POST':
+        session.delete(category)
+        session.commit()
+        return redirect(url_for('getCatalog'))
+    else:
+        return render_template('deleteCategory.html',category = category)
 
 
-@app.route('/catalog/<int:category_id>/edit/', method=['GET', 'POST'])
-def editCatalog(category_id):
-    editedCatalog = session.query(
-        Catalog).filter_by(id=category_id).one()
+@app.route('/catalog/<int:category_id>/edit/', methods=['GET', 'POST'])
+def editCategory(category_id):
+    editedCategory = session.query(
+        Category).filter_by(id=category_id).one()
     if request.method == 'POST':
         if request.form['name']:
-            editedCatalog.name = request.form['name']
-            session.add(editedCatalog)
+            editedCategory.name = request.form['name']
+            session.add(editedCategory)
             session.commit()
             return redirect(url_for('getCatalog'))
     else:
         return render_template(
-            'editCatlog.html', catalog=editedCatalog
+            'editCategory.html', category=editedCategory
         )
 
-@app.route('/item/<int:item_id>/edit/' method=['GET', 'POST'])
-def editItem(item_id):
+
+@app.route('/catalog/<int:category_id>/item/new/', methods=['GET',"POST"])
+def newItem(category_id):
+        category =  session.query(
+            Category).filter_by(id=category_id).one()
+        if request.method == 'POST':
+            newItem = Item(name=request.form['name'],
+                                   description=request.form['description'],
+                                   category_id=category_id)
+            session.add(newItem)
+            session.commit()
+            return redirect(url_for('getCategory', category_id=category_id))
+        else:
+            return render_template('newItem.html', category=category)
+
+
+@app.route('/catalog/<int:category_id>/item/<int:item_id>/edit/', methods=['GET', 'POST'])
+def editItem(item_id, category_id):
     editedItem = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
         if request.form['name']:
@@ -77,21 +98,21 @@ def editItem(item_id):
             editedItem.description = request.form['description']
         session.add(editedItem)
         session.commit()
-        return redirect(url_for('getItemInformation'), item_id = item_id)
+        return redirect(url_for('getItemInformation', item_id = item_id))
     else:
         return render_template(
-            'editeItem.html', item_id=item_id)
+            'editeItem.html', item = editedItem)
     #return 'edit item %d here' % item_id
 
 
-@app.route('/item/<int:item_id>/delete/', method=['GET','POST'])
+@app.route('/item/<int:item_id>/delete/', methods=['GET','POST'])
 def deleteItem(item_id):
     itemToDelete = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
-        catefory_id = itemToDelete.category_id
+        category_id = itemToDelete.category_id
         session.delete(itemToDelete)
         session.commit()
-        return redirect(url_for('getItemsOfCatalog'),category_id=category_id )
+        return redirect(url_for('getCategory',category_id=category_id ))
     else:
         return render_template('deleteItem.html', item=itemToDelete)
     #return 'delete item %d here' %item_id
