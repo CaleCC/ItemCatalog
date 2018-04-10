5a7c20197bbb1783168f6018c35db998532a2f2d
Cheng Chen <chencheng502@gmail.com>
2018-04-09 23:45:13 -0400

    added sign up  and styling.

--- itemcatalog.py
+++ itemcatalog.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
+from flask import session as login_session
 app = Flask(__name__)
 
 
@@ -7,6 +8,8 @@ engine = create_engine('sqlite:///catalogwithusers.db')
 
 from sqlalchemy.orm import sessionmaker
 from models import Base, User, Item, Category
+import random, string
+
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
@@ -21,14 +24,14 @@ def getCatalog():
 
 @app.route('/catalog/<int:category_id>/items')
 def getCategory(category_id):
-    catalog = session.query(Category).filter_by(id = category_id).one()
+    #catalog = session.query(Category).filter_by(id = category_id).one()
     items = session.query(Item).filter_by(category_id = category_id).all()
-    return render_template("items.html", catalog = catalog.name, items = items)
+    return render_template("items.html", category_id = category_id, items = items)
     #return 'this is the page to display items of a category %d' % category_id
 
 
 @app.route('/item/<int:item_id>')
-def getItemInformation(item_id):
+def getItem(item_id):
     item = session.query(Item).filter_by(id = item_id).one()
     catalog = session.query(Category).filter_by(id = item.category_id).one()
     return render_template("item.html", catalog = catalog, item = item)
@@ -98,10 +101,10 @@ def editItem(item_id, category_id):
             editedItem.description = request.form['description']
         session.add(editedItem)
         session.commit()
-        return redirect(url_for('getItemInformation', item_id = item_id))
+        return redirect(url_for('getItem', item_id = item_id))
     else:
         return render_template(
-            'editeItem.html', item = editedItem)
+            'editItem.html', item = editedItem)
     #return 'edit item %d here' % item_id
 
 
@@ -119,13 +122,52 @@ def deleteItem(item_id):
 
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return 'this is page for login'
+    print request.method
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        user = session.query(User).filter_by(username=name).one()
+        if user.verify_password(password):
+            state = ''.join(random.choice(string.ascii_uppercase+string.digits) for x in xrange(32))
+            login_session['state'] = state
+            login_session['username'] = name
+            print name + ' login successful'
+            return redirect('/catalog/')
+        else:
+            return 400
+    else:
+        return render_template('login.html')
+    
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method == 'POST':
+        print 'method is post'
+        name = request.form['name']
+        password = request.form['password']
+        print name 
+        print password
+        if name is None or password is None:
+            abort(400)
+        if session.query(User).filter_by(username = name).first() is not None:
+            print 'user ' + name+ ' already exist'
+            abort(400)
+        user = User(username = name)
+        user.hash_password(password)
+        session.add(user)
+        session.commit()
+        return redirect(url_for("login"))
+    else:
+        return render_template("signup.html")
+
+
 
 
 
 
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key='tsc%weTD32w3G$'
     app.run(host = '0.0.0.0', port = 8000)
